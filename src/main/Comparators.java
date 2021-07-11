6c00ec02af3a7ebd333f1a359f1923fc8a2cce99
Lynne2021 <86622973+Lynne2021@users.noreply.github.com>
2021-07-11 10:51:13 -0400

    modified some comments.

--- src/main/Comparators.java
+++ src/main/Comparators.java
@@ -12,28 +12,28 @@ public class Comparators {
      * Creat 4 new different objects instances (Number,Name,TIme,Pay)for Compare.
      * get all objects' compare data.
      */
-    //Creat new Object for compare employee number.
+    //Create new Object for compare employee number.
     public static Comparator<Employee> NUMBER = new Comparator<Employee>() {
         @Override
         public int compare(Employee o1, Employee o2) {
             return o1.getEmpNo() - o2.getEmpNo();
         }
     };
-    //Creat new Object for compare employee name.
+    //Create new Object for compare employee name.
     public static Comparator<Employee> NAME = new Comparator<Employee>() {
         @Override
         public int compare(Employee o1, Employee o2) {
             return o1.getLastName().compareTo(o2.getLastName());
         }
     };
-    //Creat new Object for compare employee worked time.
+    //Create new Object for compare employee worked time.
     public static Comparator<Employee> TIME = new Comparator<Employee>() {
         @Override
         public int compare(Employee o1, Employee o2) {
             return (int)(o2.getTimeWorked() - o1.getTimeWorked());
         }
     };
-    //Creat new Object for compare employee pay.
+    //Create new Object for compare employee pay.
     // pay = employee hourRate * worked time.
     public static Comparator<Employee> PAY = new Comparator<Employee>() {
         @Override
